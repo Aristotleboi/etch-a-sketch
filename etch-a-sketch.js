310a8726958072f51b11153edb14b9d1442faec2
aristotleboi <gage24paul@gmail.com>
2022-03-02 17:08:34 -0800

    etch-a-sketch is functioning to the products requirements. adding some css then finishing and moving on

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,35 +10,43 @@ function spawnGrid (number) {
         box.classList.add('box');
         eas.appendChild(box);
     }
+        //object with all the grids boxs
+    const gridDivs = document.querySelector('.eas').children;
+
+
+    for (var i = 0; i < gridDivs.length; i++) {
+        gridDivs[i].addEventListener('mouseover', e => {
+            let litBox = e.target;
+            litBox.style.backgroundColor = 'black';
+        });
+    }
 }
 
 spawnGrid(16);
 
-//object with all the grids boxs
-const gridDivs = document.querySelector('.eas').children;
-
-
-for (var i = 0; i < gridDivs.length; i++) {
-    gridDivs[i].addEventListener('mouseover', e => {
-        let litBox = e.target;
-        litBox.style.backgroundColor = 'black';
-    });
+function clearGrid () {
+    let border = document.querySelector('.eas');
+    let squares = border.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
 }
 
 
 
 
-
-//document.addEventListener("mouseover", e => {
-    //if (e.target.matches(".box")) {
-       // console.log(e.target)
-        //let litBox = e.target;
-       // litBox.setAttribute('id', 'litBox')
-       // eas.appendChild(litBox);
-       // console.log('hi');
-  //  }
-//})
-
-
-
+gridReset = document.querySelector('button');
+gridReset.onclick = function reset(){
+    let clickReset = prompt('Please enter a number between 1 and 100 to select the size of the Etch-A-Sketch', '16');
+    let promptNumber = parseInt(clickReset);
+    if (promptNumber !== promptNumber) {
+        alert('Selection must be a number!');
+    } else {
+        if (promptNumber < 0 || promptNumber > 100) {
+            alert('Number must be between 1 and 100');
+        } else {
+            clearGrid();
+            eas.style.gridTemplateColumns = `repeat(${promptNumber}, 1fr)`;
+            spawnGrid(promptNumber);
+        }
+    }
+}
 
